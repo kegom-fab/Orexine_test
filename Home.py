cc104fa1bde4d40fa89b501b3bfa13a9ad48dd92
kegom-fab <75729876+kegom-fab@users.noreply.github.com>
2024-04-28 17:49:38 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(
+    page_title="Opportunity",
+    page_icon="📝",
+)
+
+st.title("Welcome 😀")
+st.markdown(f'<h1 style="font-size:25px; text-align: center">{"Monitoring the evolution of income over time"}</h1>', unsafe_allow_html=True)
+
+
+
+
+
+
+st.markdown(f'<h1 style="color: blue; font-size:18px; text-align: center">{"Powerd by Orexine"}</h1>', unsafe_allow_html=True)
